290c9fee31bfc41171592e75788174c84864af28
akhilrana0001 <akhilrana0001@gmail.com>
2019-10-06 20:11:55 +0530

    button_themed

--- src/app/todo/todo-header/todo-header.component.ts
+++ src/app/todo/todo-header/todo-header.component.ts
@@ -11,40 +11,36 @@ export class TodoHeaderComponent implements OnInit {
 
   constructor(private ds:DataService) {
     setInterval(function()
-    { 
+    {
       if(ds.colorflag==1){
         (<HTMLInputElement>document.getElementById("topnav")).style.background="rgb(55, 55, 55)";
         (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle")[0]).style.background="rgb(35, 35, 35)";
         (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle-checked")[0]).style.background="rgb(55, 55, 55)";
-        // (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle")[1]).style.background="rgb(55, 55, 55)";
-        // (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle-checked")[1]).style.background="rgb(12, 12, 12)";
 
       }
       else{
         (<HTMLInputElement>document.getElementById("topnav")).style.background="rgb(26, 58, 85)";
-        // (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle")[0]).style.background="rgb(163, 163, 163)";
         (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle-checked")[0]).style.background="rgb(163, 163, 163)";
          (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle")[1]).style.background="rgb(135, 135, 135)";
-        // (<HTMLInputElement>document.getElementsByClassName("mat-button-toggle-checked")[1]).style.background="rgb(112, 112, 112)";
       }
     }, 200);
    }
 
 
   ngOnInit() {
-   
+
   }
   dark(){
     (<HTMLInputElement>document.getElementById("body")).style.background="rgb(31, 31, 31)";
     this.ds.colorflag=1;
-    
+
 
   }
-  
+
   light(){
     (<HTMLInputElement>document.getElementById("body")).style.background="rgb(122, 172, 177)";
     this.ds.colorflag=0;
 
-  } 
-  
+  }
+
 }
